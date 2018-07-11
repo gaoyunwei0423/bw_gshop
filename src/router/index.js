5cebc45f0d1c23af9c03eca21636671f4a214698
unknown <m13453867591@163.com>
2018-07-11 14:29:06 +0800

    init  gshop

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+/*
+路由器模块
+*/
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Msite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({ routes: [
+    {
+      path: '/', redirect: '/msite'
+    }, {
+      path: '/msite',
+      component: Msite,
+      meta: {
+        showFooter: true//控制组件路由是否显示
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    }]
+})
